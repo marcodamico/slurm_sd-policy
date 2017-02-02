5718be4d4423caabc3ed7c22596cb46bf3f12c8b
MARCO D'AMICO <marco.damico@bsc.es>
2017-02-02 16:12:18 +0100

    [b1ca57aa1bef589d6d7e3b7d6dc8c9d4a89d01f7] bugfix

--- src/plugins/select/linear/select_linear.c
+++ src/plugins/select/linear/select_linear.c
@@ -607,7 +607,7 @@ static int _job_count_bitmap(struct cr_record *cr_ptr,
 			gres_cpus *= cpus_per_core;
 			if ((gres_cpus < cpu_cnt) ||
 			    (gres_cpus < job_ptr->details->ntasks_per_node) ||
-			    ((job_ptr->details->t_ffscpus_per_task > 1) &&
+			    ((job_ptr->details->cpus_per_task > 1) &&
 			     (gres_cpus < job_ptr->details->cpus_per_task))) {
 				bit_clear(jobmap, i);
 				continue;
@@ -650,38 +650,40 @@ static int _job_count_bitmap(struct cr_record *cr_ptr,
 		}
 		/* Marco: discard nodes where is not possible to steal more cpus */
 		slurm_ctl_conf_t *config = slurm_conf_lock();
-                DLB_share_factor = config->sharing_factor;
+                uint16_t DLB_share_factor = config->sharing_factor;
                 slurm_conf_unlock();
 		ListIterator job_iterator;
 		struct job_record *job_ptr;
-		uint16_t cores_stealable = cores_cnt * DLB_share_factor;
-		uint16_t cores_free = cores_cnt;
-		uint16_t cores_missing;
+		uint16_t cpu_stealable = cpu_cnt * DLB_share_factor;
+		uint16_t cpu_free = cpu_cnt;
+		uint16_t cpu_missing;
+		uint16_t cpu_requested;
 		
 		job_iterator = list_iterator_create(job_list);
-		while ((job_ptr = (struct job_record *) list_next(job_iterator)) && cores_stealable > 0) {
+		while ((job_ptr = (struct job_record *) list_next(job_iterator)) && cpu_stealable > 0) {
 			if (!IS_JOB_RUNNING(job_ptr)) {
 				debug("Job %d not running", job_ptr->job_id);
 				continue;
+			}
 			if(job_ptr->node_bitmap && bit_test(job_ptr->node_bitmap, i)) {
-				cores_requested = job_ptr->cpus_per_task * job_ptr->ntasks_per_node;
+				cpu_requested = job_ptr->details->cpus_per_task * job_ptr->details->ntasks_per_node;
 				debug("Job %d running on node %d", job_ptr->job_id, i);
 				//should I consider Jobs that start with reduced cpt
-				if (cores_requested < cores_free)
-                                        cores_free -= cores_requested;
+				if (cpu_requested < cpu_free)
+                                        cpu_free -= cpu_requested;
                                 else { //steal was necessary
-                                        cores_missing = cores_requested - cores_free;
-					while (cores_missing > cores_stealable)
-						cores_missing -= job_ptr->ntasks_per_node; 
-                                        assert((cores_missing + cores_free) >= job_record_ptr->ntasks_per_node) //if node was allocated for this job means
+                                        cpu_missing = cpu_requested - cpu_free;
+					while (cpu_missing > cpu_stealable)
+						cpu_missing -= job_ptr->details->ntasks_per_node; 
+                                        assert((cpu_missing + cpu_free) >= job_record_ptr->ntasks_per_node);    //if node was allocated for this job means
 														//that we allocated at least the min 
 														//number of cpus (ntasks_per_node)
-                                        cores_stealable -= cores_missing;
-                                        cores_free = 0;
+                                        cpu_stealable -= cpu_missing;
+                                        cpu_free = 0;
                                 }
 			}
 		}
-		if(cores_free + cores_stealable > job_ptr->ntasks_per_node) {
+		if(cpu_free + cpu_stealable > job_ptr->details->ntasks_per_node) {
 			bit_clear(jobmap, i);
 			debug("Job %d cannot be allocated on node %d for lack of stealable cpus", job_ptr->job_id, i);
 			continue;
@@ -820,7 +822,7 @@ static int _job_test(struct job_record *job_ptr, bitstr_t *bitmap,
 
 	for (index = 0; index < select_node_cnt; index++) {
 		if (bit_test(bitmap, index)) {
-			if (consec_nodet_fit_cpus[consec_index] == 0)
+			if (consec_nodes[consec_index] == 0)
 				consec_start[consec_index] = index;
 			avail_cpus = _get_avail_cpus(job_ptr, index);
 			if (job_ptr->details->req_node_bitmap	&&
