5274fb49209420f51f6a06d5dd4d1df06ecee0ce
Marco D'Amico <mdamico@bsc.es>
2018-09-07 17:39:51 +0200

    Fix for f36cd101821e1dbed0ad99534269d1d131c55e11 with duplicated code

--- src/slurmctld/controller.c
+++ src/slurmctld/controller.c
@@ -1753,7 +1753,7 @@ static void *_slurmctld_background(void *no_data)
 */
 		if(job_count) {
 			slurmctld_diag_stats.avg_slowdown = (double) slowdown_acc / job_count;
-			fprintf(stats,"Average slowdown for running jobs: %f", slurmctld_diag_stats.avg_slowdown);
+//			fprintf(stats,"Average slowdown for running jobs: %f", slurmctld_diag_stats.avg_slowdown);
 			debug("Average slowdown for running jobs: %f", slurmctld_diag_stats.avg_slowdown);
 //			int median = (double) job_count * 0.5f;
 //			int perc70 = (double) job_count * 0.7f;
@@ -1766,25 +1766,6 @@ static void *_slurmctld_background(void *no_data)
 
 		now = time(NULL);
                 START_TIMER;
-
-		/* Marco: calculate avg wait time of running jobs */
-                ListIterator job_iterator;
-                struct job_record *job_ptr = NULL;
-                uint32_t wait_time_acc = 0;
-                uint32_t job_count = 0;
-
-                lock_slurmctld(job_node_read_lock);
-                job_iterator = list_iterator_create(job_list);
-                while ((job_ptr = list_next(job_iterator))) {
-                        if ((!IS_JOB_PENDING(job_ptr)) ) {
-                                wait_time_acc += job_ptr->start_time - job_ptr->details->submit_time;
-                                job_count++;
-                        }
-                }
-                list_iterator_destroy(job_iterator);
-                unlock_slurmctld(job_node_read_lock);
-                slurmctld_diag_stats.avg_wait_time = (double) wait_time_acc / job_count;
-                debug("Average wait time for jobs: %f", slurmctld_diag_stats.avg_wait_time);
 
 		if (slurmctld_conf.slurmctld_debug <= 3)
 			no_resp_msg_interval = 300;
